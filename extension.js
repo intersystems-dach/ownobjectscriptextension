b0e8cd49e29e086c36d732a0cd978bb5f4a5ebb0
phil1436 <philxsb@gmail.com>
2022-11-30 16:22:44 +0100

    v0.0.3

--- extension.js
+++ extension.js
@@ -11,22 +11,11 @@ const keyWords = optionsJSON['KeyWords'];
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  //Add ObjectScript Modifier
   let disposable = vscode.commands.registerCommand(
     'ownobjectscriptextension.addObjectScriptModifier',
     function () {
-      // Check if there is an active TexteEditor
-      if (vscode.window.activeTextEditor == undefined) {
-        vscode.window.showErrorMessage(
-          'Please open an ObjectScript file first!'
-        );
-        return;
-      }
-
-      // Check if TextEditor is ObjectScript
-      if (!vscode.window.activeTextEditor.document.fileName.endsWith('.cls')) {
-        vscode.window.showErrorMessage('Only works with ObjectScript files!');
-        return;
-      }
+      preConditions();
 
       vscode.window.activeTextEditor.edit(function (editBuilder) {
         let modifiedLines = [];
@@ -60,6 +49,7 @@ function activate(context) {
             i = skipUnitlToken(i, '(', ')');
           }
         }
+
         if (!optionsJSON['ShowMessages']) return;
         // Show message
         if (modifiedLines.length == 0)
@@ -79,22 +69,64 @@ function activate(context) {
     }
   );
 
+  //toggle show message
   vscode.commands.registerCommand(
     'ownobjectscriptextension.toggleShowMessages',
     function () {
       optionsJSON['ShowMessages'] = !optionsJSON['ShowMessages'];
+      vscode.window.setStatusBarMessage(
+        'Show Message: ' + (optionsJSON['ShowMessages'] ? 'ON' : 'OFF'),
+        4000
+      );
+
+      /* vscode.window
+        .showInformationMessage(
+          'Show Message: ' + (optionsJSON['ShowMessages'] ? 'ON' : 'OFF'),
+          ...[optionsJSON['ShowMessages'] ? 'Turn OFF' : 'Turn ON']
+        )
+        .then((item) => {
+          if (item == 'Turn OFF') {
+            optionsJSON['ShowMessages'] = false;
+            saveOptions();
+          } else if (item == 'Turn ON') {
+            optionsJSON['ShowMessages'] = true;
+            saveOptions();
+          }
+        }); */
+
       saveOptions();
     }
   );
 
+  //toggle save file
   vscode.commands.registerCommand(
     'ownobjectscriptextension.toggleSaveFile',
     function () {
       optionsJSON['SaveFile'] = !optionsJSON['SaveFile'];
+      vscode.window.setStatusBarMessage(
+        'Save file: ' + (optionsJSON['SaveFile'] ? 'ON' : 'OFF'),
+        4000
+      );
+
+      /* vscode.window
+        .showInformationMessage(
+          'Save file: ' + (optionsJSON['SaveFile'] ? 'ON' : 'OFF'),
+          ...[optionsJSON['SaveFile'] ? 'Turn OFF' : 'Turn ON']
+        )
+        .then((item) => {
+          if (item == 'Turn OFF') {
+            optionsJSON['SaveFile'] = false;
+            saveOptions();
+          } else if (item == 'Turn ON') {
+            optionsJSON['SaveFile'] = true;
+            saveOptions();
+          }
+        }); */
       saveOptions();
     }
   );
 
+  //add keyword
   vscode.commands.registerCommand(
     'ownobjectscriptextension.addKeyWord',
     function () {
@@ -103,15 +135,19 @@ function activate(context) {
       });
       input.then(function (value) {
         if (value == undefined) return;
-        value = value.toLowerCase().replace(/\s/g, '');
-        if (keyWords.includes(value)) return;
-        keyWords.push(value);
+        let valueTrimmed = value.toLowerCase().replace(/\s/g, '');
+        if (keyWords.includes(valueTrimmed)) return;
+        keyWords.push(valueTrimmed);
         saveOptions();
-        displayKeyWords();
+        vscode.window.setStatusBarMessage(
+          'KeyWord "' + value + '" added',
+          4000
+        );
       });
     }
   );
 
+  //remove keyword
   vscode.commands.registerCommand(
     'ownobjectscriptextension.removeKeyWord',
     function () {
@@ -128,20 +164,243 @@ function activate(context) {
           keyWords.splice(index, 1); // 2nd parameter means remove one item only
         }
         saveOptions();
-        displayKeyWords();
+        vscode.window.setStatusBarMessage(
+          'KeyWord "' + value + '" removed',
+          4000
+        );
       });
     }
   );
 
+  //show keywords
   vscode.commands.registerCommand(
     'ownobjectscriptextension.showKeyWords',
-    displayKeyWords
+    function () {
+      let s = '';
+      for (let key in keyWords) s += keyWords[key] + ' , ';
+      vscode.window
+        .showInformationMessage(s, ...['Add', 'Remove'])
+        .then((item) => {
+          if (item == 'Add') {
+            vscode.commands.executeCommand(
+              'ownobjectscriptextension.addKeyWord'
+            );
+          } else if (item == 'Remove') {
+            vscode.commands.executeCommand(
+              'ownobjectscriptextension.removeKeyWord'
+            );
+          }
+          console.log(item);
+        });
+    }
   );
+
+  //open settings
+  vscode.commands.registerCommand(
+    'ownobjectscriptextension.openSettings',
+    function () {
+      vscode.workspace
+        .openTextDocument(vscode.Uri.file(optionsFile))
+        .then((a) => {
+          vscode.window.showTextDocument(a, 1, false);
+        });
+    }
+  );
+
+  //add method description template
+  vscode.commands.registerCommand(
+    'ownobjectscriptextension.addMethodDescriptionTemplate',
+    function () {
+      preConditions();
+      let startLine = undefined;
+      let lineAbove = undefined;
+      // Find method
+      for (
+        let i = vscode.window.activeTextEditor.selection.start.line;
+        i >= 0;
+        i--
+      ) {
+        let line = vscode.window.activeTextEditor.document.lineAt(i);
+        let trimedLine = line.text.toLowerCase().replace(/\s/g, '');
+        if (
+          trimedLine.startsWith('method') ||
+          trimedLine.startsWith('classmethod')
+        ) {
+          startLine = line;
+          if (i == 0) break;
+          let lineAboveTmp = vscode.window.activeTextEditor.document.lineAt(
+            i - 1
+          );
+          let trimmedLineAbove = lineAboveTmp.text
+            .toLowerCase()
+            .replace(/\s/g, '');
+          if (trimmedLineAbove.startsWith('///')) {
+            if (
+              trimmedLineAbove == '///description' ||
+              trimmedLineAbove == '///'
+            ) {
+              //Remove description if no valid comment
+              lineAbove = lineAboveTmp;
+              break;
+            } else {
+              //Return if comment already exists
+              return;
+            }
+          }
+          break;
+        }
+      }
+
+      if (startLine == undefined) {
+        vscode.window.showErrorMessage('No method found!');
+        return;
+      }
+      let template = '';
+
+      //Add method description
+      for (let i in optionsJSON['MethodCommentTemplate']['MethodDescription']) {
+        template +=
+          '/// ' +
+          optionsJSON['MethodCommentTemplate']['MethodDescription'][i] +
+          '\n';
+      }
+
+      // add paremeter description
+      template += makeParemterTemplate(startLine);
+
+      //if has return value
+      if (startLine.text.split(')')[1].toLowerCase().includes('as')) {
+        // Add return description
+        for (let i in optionsJSON['MethodCommentTemplate'][
+          'ReturnDescription'
+        ]) {
+          template +=
+            '/// ' +
+            optionsJSON['MethodCommentTemplate']['ReturnDescription'][i] +
+            '\n';
+        }
+      }
+      // Add example
+      if (startLine.text.toLowerCase().includes('classmethod')) {
+        for (let i in optionsJSON['MethodCommentTemplate'][
+          'ExampleClassMethod'
+        ]) {
+          template +=
+            '/// ' +
+            optionsJSON['MethodCommentTemplate']['ExampleClassMethod'][i] +
+            '\n';
+        }
+      } else {
+        for (let i in optionsJSON['MethodCommentTemplate']['ExampleMethod']) {
+          template +=
+            '/// ' +
+            optionsJSON['MethodCommentTemplate']['ExampleMethod'][i] +
+            '\n';
+        }
+      }
+
+      //get classname
+      let className = vscode.window.activeTextEditor.document.fileName.replace(
+        '.cls',
+        ''
+      );
+      className = ownReplaceAll(className, '\\', '.');
+      if (className.startsWith('.')) className = className.replace('.', '');
+
+      //get method name
+      let methodName = startLine.text.split(' ')[1];
+      if (methodName.includes('(')) methodName = methodName.split('(')[0];
+
+      //replace spaceholder
+      template = ownReplaceAll(template, '*classname*', className);
+      template = ownReplaceAll(template, '*methodname*', methodName);
+
+      vscode.window.activeTextEditor.edit(function (editBuilder) {
+        if (lineAbove != undefined) editBuilder.delete(lineAbove.range);
+        editBuilder.insert(startLine.range.start, template);
+      });
+
+      //Save if option is turned on
+      if (optionsJSON['SaveFile'])
+        vscode.window.activeTextEditor.document.save();
+    }
+  );
+
   context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
+
+/**
+ * @param {string} s
+ * @param {string} searchString
+ * @param {string} replaceString
+ */
+function ownReplaceAll(s, searchString, replaceString) {
+  while (s.includes(searchString)) s = s.replace(searchString, replaceString);
+  return s;
+}
+
+/**
+ * @param {vscode.TextLine} startLine
+ */
+function makeParemterTemplate(startLine) {
+  let parameterRaw = startLine.text.split('(')[1];
+  let parameterArray = parameterRaw.split(')')[0].split(',');
+  let parameterTemplate = '';
+  //Add parameter
+  for (let i in parameterArray) {
+    while (true) {
+      if (parameterArray[i].charAt(0) == ' ') {
+        parameterArray[i] = parameterArray[i].replace(' ', '');
+      } else {
+        break;
+      }
+    }
+    let name = parameterArray[i].split(' ')[0];
+    if (name.toLowerCase() == 'byref' || name.toLowerCase() == 'output')
+      name = parameterArray[i].split(' ')[1];
+    if (name != '') {
+      let isOptional = parameterArray[i].includes('=') ? true : false;
+      for (let j in optionsJSON['MethodCommentTemplate'][
+        'ParameterDescription'
+      ]) {
+        let temp =
+          optionsJSON['MethodCommentTemplate']['ParameterDescription'][j];
+        temp = ownReplaceAll(temp, '*parametername*', name);
+        temp = ownReplaceAll(
+          temp,
+          '*optional*',
+          isOptional ? '(optional)' : ''
+        );
+        parameterTemplate += '/// ' + temp + '\n';
+      }
+      /* parameterTemplate +=
+        '/// <li>' +
+        name +
+        (parameterArray[i].includes('=') ? '(optional)' : '') +
+        ': ' +
+        name +
+        'Description</li>\n'; */
+    }
+  }
+  return parameterTemplate;
+}
+
+function preConditions() {
+  // Check if there is an active TexteEditor
+  if (vscode.window.activeTextEditor == undefined) {
+    vscode.window.showErrorMessage('Please open an ObjectScript file first!');
+    return;
+  }
+
+  // Check if TextEditor is ObjectScript
+  if (!vscode.window.activeTextEditor.document.fileName.endsWith('.cls')) {
+    vscode.window.showErrorMessage('Only works with ObjectScript files!');
+    return;
+  }
+}
 
 /**
  * @param {number} index
@@ -201,14 +460,11 @@ function addModifier(line, firstIndex) {
   return line.slice(0, firstIndex) + 'Do ' + line.slice(firstIndex);
 }
 
+/**
+ *
+ */
 function saveOptions() {
   fs.writeFileSync(optionsFile, JSON.stringify(optionsJSON, null, 2));
-}
-
-function displayKeyWords() {
-  let s = '';
-  for (let key in keyWords) s += keyWords[key] + ', ';
-  vscode.window.showInformationMessage(s);
 }
 
 module.exports = {
